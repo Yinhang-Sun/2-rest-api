dac99f17719325cb7a31542bb42ffbdeac49c4dc
yinhang_sun <sunyinhang@gmail.com>
2023-01-28 09:25:01 -0800

    Adding Update Todo and Create Todo REST API to Spring Boot Backend API

--- src/main/java/com/yinhang/rest/webservices/restapi/todo/TodoService.java
+++ src/main/java/com/yinhang/rest/webservices/restapi/todo/TodoService.java
@@ -15,7 +15,6 @@ public class TodoService {
 	private static int todosCount = 0;
 	
 	static {
-		
 		todos.add(new Todo(++todosCount, "Yinhang","Get AWS Certified", 
 							LocalDate.now().plusYears(10), false ));
 		todos.add(new Todo(++todosCount, "Yinhang","Learn DevOps", 
